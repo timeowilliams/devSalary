5b4240bfb1ed929d0e07a088c66c64045e1a93f5
Anthony <tonegiovanni@gmail.com>
2021-04-20 10:44:09 -0400

    Adds user route.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const userRouter = require('./routes/user');
 
 app.use(express.json());
 
@@ -9,6 +10,24 @@ app.use('/build', express.static(path.resolve(__dirname, '../build')));
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
 });
+
+// cache
+
+// users/authenticate - should be first to set tokens when necessary
+app.use('/user', userRouter);
+// states
+// applications
+
+/*
+
+for states
+for updates
+for users
+
+*/
+
+// route to server
+// app.use()
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
