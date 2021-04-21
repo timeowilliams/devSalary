b8bbca72f78d51026b1013ae68e4d1a645f2b336
Anthony <tonegiovanni@gmail.com>
2021-04-21 02:03:28 -0400

    Fixes route testing issue. Adds API to server/app.js. Exports API as app. Requires app into server/server.js. Requires app into tests for application routes.

--- __tests__/__config__/jest.setup.js
+++ __tests__/__config__/jest.setup.js
@@ -1,5 +1,5 @@
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-17-updated');
+const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,44 +1,5 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const userRouter = require('./routes/user');
-const statesRouter = require('./routes/states');
-const applicationsRouter = require('./routes/applications');
-
-app.disable('x-powered-by');
-
-app.use(express.json());
-
-app.use('/build', express.static(path.resolve(__dirname, '../build')));
-
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
-});
-
-// cache
-
-// users/authenticate - should be first to set tokens when necessary
-app.use('/user', userRouter);
-// states
-app.use('/states', statesRouter);
-// applications
-app.use('/applications', applicationsRouter);
-
-// Global error handler
-app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  return res.status(errorObj.status).json(errorObj.message);
-});
-
-// 404 handler
+const app = require('./app');
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
-
-module.exports = app;
