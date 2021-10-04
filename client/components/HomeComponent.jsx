f90869b53fcb022e4f0d139a8fe868c4c49a2cb3
Timeo <timeo.j.williams@gmail.com>
2021-10-04 17:07:43 -0400

    Readded DataMap component to be served

--- client/components/HomeComponent.jsx
+++ client/components/HomeComponent.jsx
@@ -1,13 +1,19 @@
 import LoginComponent from "./LoginComponent";
 import BodyImage from '../../public/assets/homePhoto.jpeg'
-// import './HomeComponent.css';
+ import './HomeComponent.css';
 import React from "react";
 
 const HomeComponent = () => {
   return (
-  <div>
-   <img src={BodyImage} style={{width: 1000, height: 500 }}/>
-  <LoginComponent />
+  <div style={{
+    position: 'relative',
+  }}>
+   <img src={BodyImage} style={{width: `100%`, height: `50%` }}/>
+  <LoginComponent style={{
+    position: 'absolute',
+    bottom: '8px',
+    left: '16px'
+  }} />
   </div>
   )
 }
--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -16,13 +16,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <center> <h3>Democratizing software developer salary info so you don't have too</h3></center>
-        <LoginComponent/> */}
         <HomeComponent />
         <div className='datamap-outer-conainer'>
-        {/* <DataMap 
+        <DataMap 
         regionData={this.props.regionData} 
-        /> */}
+        />
         <div className = 'data'></div>
       </div>
       </div>
